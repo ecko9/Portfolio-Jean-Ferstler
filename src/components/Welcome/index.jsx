6735bd3380cb4cf0723171ca8035a22922b5d48d
ecko9 <jean.ferstler@gmail.com>
2022-02-02 20:10:08 +0100

    init portfolio files + test animation

---
+++ src/components/Welcome/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div className='Welcome'>
+
+      <div className='big-cube'>
+        <div className='cube c1' >
+          <div className="triangle t1" />
+          <div className="triangle t2" />
+          <div className="triangle t3" />
+          <div className="triangle t4" />
+        </div>
+        <div className='cube c2' >
+          <div className="triangle t1" />
+          <div className="triangle t2" />
+          <div className="triangle t3" />
+          <div className="triangle t4" />
+        </div>
+        <div className='cube c3' >
+          <div className="triangle t1" />
+          <div className="triangle t2" />
+          <div className="triangle t3" />
+          <div className="triangle t4" />
+        </div>
+        <div className='cube c4' >
+          <div className="triangle t1" />
+          <div className="triangle t2" />
+          <div className="triangle t3" />
+          <div className="triangle t4" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Welcome from '../components/Welcome';
+
+const Home = () => {
+  return (
+    <div className='Home'>
+      <Welcome />
+    </div>
+  );
+};
+
+export default Home;

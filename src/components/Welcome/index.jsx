a69bc22d499e1884166226620375acc4e0781527
ecko9 <jean.ferstler@gmail.com>
2022-02-03 18:52:46 +0100

    init files 2

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -10,24 +10,44 @@ const Welcome = () => {
           <div className="triangle t2" />
           <div className="triangle t3" />
           <div className="triangle t4" />
+
+          <div className='triangle t1-b' />
+          <div className='triangle t2-b' />
+          <div className='triangle t3-b' />
+          <div className='triangle t4-b' />
         </div>
         <div className='cube c2' >
           <div className="triangle t1" />
           <div className="triangle t2" />
           <div className="triangle t3" />
           <div className="triangle t4" />
+
+          <div className='triangle t1-b' />
+          <div className='triangle t2-b' />
+          <div className='triangle t3-b' />
+          <div className='triangle t4-b' />
         </div>
         <div className='cube c3' >
           <div className="triangle t1" />
           <div className="triangle t2" />
           <div className="triangle t3" />
           <div className="triangle t4" />
+
+          <div className='triangle t1-b' />
+          <div className='triangle t2-b' />
+          <div className='triangle t3-b' />
+          <div className='triangle t4-b' />
         </div>
         <div className='cube c4' >
           <div className="triangle t1" />
           <div className="triangle t2" />
           <div className="triangle t3" />
           <div className="triangle t4" />
+
+          <div className='triangle t1-b' />
+          <div className='triangle t2-b' />
+          <div className='triangle t3-b' />
+          <div className='triangle t4-b' />
         </div>
       </div>
     </div>

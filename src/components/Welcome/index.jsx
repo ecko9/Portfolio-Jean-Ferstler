06c78c36816150063582f8688dbf2ca8cbe68b9f
ecko9 <jean.ferstler@gmail.com>
2022-02-05 03:20:35 +0100

    refacto first bg animation

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -2,78 +2,71 @@ import React from 'react';
 
 const Welcome = () => {
 
-  const [linesNumber, setLinesNumber] = React.useState(null)
-  const [linesWidth, setLinesWidth] = React.useState(null)
+  const [linesNumber, setLinesNumber] = React.useState(8)
+  const [bgSize, setBgSize] = React.useState(null)
   const [animationsDelayTimes, setAnimationDelayTimes] = React.useState([1, 11, 5, 20, 3, 9, 16, 10, 2, 7, 1, 11, 5, 20, 3, 9, 16, 10, 2, 7])
 
   React.useEffect(
     () => {
-      const determineLinesNumber = () => {
-        let linesWidthTmp
-
-        if (window.screen.width < window.screen.height) {
-          linesWidthTmp = Math.floor(window.screen.width / 6)
-          setLinesNumber(Math.floor(window.screen.height * 2 / linesWidthTmp))
-        }
-        else {
-          linesWidthTmp = Math.floor(window.screen.height / 6)
-          setLinesNumber(Math.floor(window.screen.width * 2 / linesWidthTmp))
-        }
-
-        setLinesWidth(linesWidthTmp)
+      const determineDiagonal = () => {
+        let hypothenuse = Math.floor(Math.pow(Math.floor(Math.pow(window.screen.width, 2) + Math.pow(window.screen.height, 2)), 0.5))
+        setBgSize(hypothenuse)
       }
 
-      determineLinesNumber()
+      determineDiagonal()
       return;
     }, []
   )
   return (
     <div className='Welcome' >
 
-      {linesWidth !== null && linesNumber !== null &&
-        <div className='bg-lines' >
+      {bgSize !== null &&
+        <div
+          className='bg-lines'
+          style={{
+            width: `${bgSize}px`,
+            height: `${bgSize}px`,
+            top: `-${Math.floor((bgSize - window.screen.height) / 2)}px`,
+            left: `-${Math.floor((bgSize - window.screen.width) / 2)}px`
+          }}
+        >
           {Array.from({ length: linesNumber }).map((n, i) => (
             <div
               className='line'
-              style={{ width: `100%`, height: `${linesWidth}px` }}
               key={i}
             >
               <div className='line-overlay animation-left-and-right' style={{ animationDelay: `${animationsDelayTimes[i]}s` }}>
-                <div className='adjust-block' />
                 {Array.from({ length: linesNumber }).map((n, i) => (
-                  <div
-                    className='cube'
-                    style={{ width: `${linesWidth}px`, height: `100%` }}
-                    key={i}
-                  >
+                  <div className='cube' key={i}>
                   </div>
                 ))}
               </div>
+
             </div>
           ))}
         </div>
       }
 
-      {linesWidth !== null && linesNumber !== null &&
-        <div className='bg-col ' >
+      {bgSize !== null &&
+        <div
+          className='bg-col '
+          style={{
+            width: `${bgSize}px`,
+            height: `${bgSize}px`,
+            top: `-${Math.floor((bgSize - window.screen.height) / 2)}px`,
+            left: `-${Math.floor((bgSize - window.screen.width) / 2)}px`
+          }}
+        >
           {Array.from({ length: linesNumber }).map((n, i) => (
-            <div
-              className='col'
-              style={{ width: `${linesWidth}px`, height: `100%` }}
-              key={i}
-            >
+            <div className='col' key={i} >
+
               <div className='col-overlay animation-up-and-down' style={{ animationDelay: `${animationsDelayTimes[i]}s` }}>
-                <div className='adjust-block' />
                 {Array.from({ length: linesNumber }).map((n, i) => (
-                  <div
-                    className='cube'
-                    style={{ width: `100%`, height: `${linesWidth}px` }}
-                    key={i}
-                  >
+                  <div className='cube' key={i} >
                   </div>
                 ))}
-
               </div>
+
             </div>
           ))}
         </div>

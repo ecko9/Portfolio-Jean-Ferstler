acea049a8138cab01321c312705a150f886d1b27
ecko9 <jean.ferstler@gmail.com>
2022-02-22 17:23:32 +0100

    change cube colors

--- src/components/Background/Cube/index.jsx
+++ src/components/Background/Cube/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Cube = ({ i2, animationsDelayTimes }) => {
 
   const [indexColor, setIndexColor] = React.useState(0)
-  const animationsColors = ["black", "rgb(150, 150, 150)", "rgb(200, 200, 200)", "white", "rgba(255, 184, 92, 0.8)", "rgba(92, 255, 179, 0.8)", "rgba(92, 95, 255, 0.8)", "rgba(255, 92, 133, 0.8)", "rgba(255, 138, 92, 0.8)", "rgba(248, 226, 26, 0.8)"];
+  const animationsColors = ["rgba(255, 184, 92, 0.8)", "rgba(92, 255, 179, 0.8)", "rgba(92, 95, 255, 0.8)", "rgba(255, 92, 133, 0.8)", "rgba(255, 138, 92, 0.8)", "rgba(248, 226, 26, 0.8)"];
 
   const getRandomInt = (max) => {
     return (Math.floor(Math.random() * max))
